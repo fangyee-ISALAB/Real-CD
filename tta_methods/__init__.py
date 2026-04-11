1a6f244b4e491d94b71ed8be7448d399a7df4c77
fangyee_ISALAB <157097638+fangyee-ISALAB@users.noreply.github.com>
2026-04-12 00:14:44 +0800

    Update __init__.py

--- tta_methods/__init__.py
+++ tta_methods/__init__.py
@@ -1,14 +1,7 @@
 from tta_methods.source import Source
-from tta_methods.cotta import CoTTA
-from tta_methods.memo import MEMO
 from tta_methods.tent import Tent
-from tta_methods.sar import SAR
-from tta_methods.rpl import RPL
-from tta_methods.roid import ROID
-from tta_methods.cmf import CMF
 from tta_methods.Ours import TTTA
-from tta_methods.recap import ReCAP
 
 __all__ = [
-    'Source', 'CoTTA', 'MEMO', 'Tent', 'SAR', 'RPL', 'ROID', 'CMF', 'TTTA', 'ReCAP'
+    'Source', 'Tent', 'TTTA'
 ]
